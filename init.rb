3d27320f0ae2e2af0e04d3c0d552ecd33c099ce9
leanderkretschmer <leanderkretschmer@icloud.com>
2025-08-29 18:08:11 +0200

    v1.5.3

--- init.rb
+++ init.rb
@@ -6,12 +6,24 @@ Redmine::Plugin.register :mail_receiver do
   name 'Mail Receiver Plugin'
   author 'leanderkretschmer'
   description 'IMAP Mail Receiver + Reminder Scheduler for Redmine'
-  version '1.5.2'
+  version '1.5.3'
   url 'https://github.com/leanderkretschmer/redmine_mail_receiver'
   author_url 'https://github.com/leanderkretschmer'
 
   # Verwende plugin-interne Konfiguration statt Redmine-Einstellungen
   settings default: {}, partial: 'mail_receiver_settings/edit'
+end
+
+# Routen für das Plugin registrieren
+Rails.application.config.after_initialize do
+  Rails.application.routes.draw do
+    resources :mail_receiver_settings, only: [:edit, :update] do
+      collection do
+        post :manual_import
+        post :clear_log
+      end
+    end
+  end
 end
 
 unless defined?(Rails::Console) || File.split($0).last == 'rake'
--- routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :mail_receiver_settings, only: [:edit, :update] do
-    collection do
-      post :manual_import
-      post :clear_log
-    end
-  end
-end
