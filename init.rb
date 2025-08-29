3b4875f24223b28a9878615d1b8ede44a90a7539
leanderkretschmer <leanderkretschmer@icloud.com>
2025-08-29 18:24:57 +0200

    v1.5.5

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :mail_receiver do
   name 'Mail Receiver Plugin'
   author 'leanderkretschmer'
   description 'IMAP Mail Receiver + Reminder Scheduler for Redmine'
-  version '1.5.4'
+  version '1.5.5'
   url 'https://github.com/leanderkretschmer/redmine_mail_receiver'
   author_url 'https://github.com/leanderkretschmer'
 
@@ -15,7 +15,10 @@ Redmine::Plugin.register :mail_receiver do
   settings default: {}, partial: 'mail_receiver_settings/edit'
 end
 
-
+# Lokalisierungsdateien laden
+Rails.application.config.after_initialize do
+  I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'locales', '*.yml')]
+end
 
 unless defined?(Rails::Console) || File.split($0).last == 'rake'
   Rails.application.config.after_initialize do
